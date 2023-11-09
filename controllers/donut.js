af9e41adba86f9ab61fee33fd575fb119aaa37bf
Meghanaankam16 <143150657+Meghanaankam16@users.noreply.github.com>
2023-11-08 22:30:17 -0600

    added part 8

--- controllers/donut.js
+++ controllers/donut.js
@@ -44,3 +44,24 @@ exports.donut_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+// Handle Costume create on POST.
+exports.donut_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new donut();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.donut_name = req.body.donut_name;
+    document.donut_price = req.body.donut_price;
+    document.donut_store = req.body.donut_store;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
