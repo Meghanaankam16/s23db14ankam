6c6ac98a6b1cef9be2d301e57e24a59f475821d3
Meghanaankam16 <143150657+Meghanaankam16@users.noreply.github.com>
2023-11-15 20:02:14 -0600

    added part 4

--- controllers/donut.js
+++ controllers/donut.js
@@ -98,3 +98,34 @@ ${JSON.stringify(req.body)}`)
 failed`);
     }
 };
+
+//ss4
+// Handle Costume delete on DELETE.
+exports.donut_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await donut.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+  
+
+    //ss5
+    // Handle a show one view with id specified by query
+    exports.donut_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await donut.findById( req.query.id)
+    res.render('donutdetail',
+    { title: 'Donut Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
--- routes/donut.js
+++ routes/donut.js
@@ -3,4 +3,5 @@ const donut_controlers= require('../controllers/donut');
 var router = express.Router();
 /* GET costumes */
 router.get('/', donut_controlers.donut_view_all_Page );
+router.get('/detail', donut_controlers.donut_view_one_Page);
 module.exports = router;
